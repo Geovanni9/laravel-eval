187788304034d183f9bc1357448278c6a6934684
Geovanni <g.nsungoue@it-students.fr>
2021-03-12 17:03:06 +0100

    display update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\HeroesController;
 */
 
 Route::get('/', function () { 
-    return view('welcome');
+    return view('create');
 });
 
 Route::resource('heroes', HeroesController::class);
